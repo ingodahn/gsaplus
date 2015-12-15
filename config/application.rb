941c6d88d94e8bb3d7c90ce69687a563784241bb
turbopope <PapstDonB@Googlemail.com>
2015-12-15 15:02:45 +0100

    Include bower-dependencies in asset-pipeline

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module GSAOnlinePlus
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Include bower-dependencies in asset-pipeline
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
