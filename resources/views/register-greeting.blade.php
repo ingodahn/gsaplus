a84ef338d2a0056060655b4a24a3110172b94e39
Marco Brack <PapstDonB@Googlemail.com>
2016-01-12 21:45:23 +0100

    Add breadcrumbs to register-greeting view

--- resources/views/register-greeting.blade.php
+++ resources/views/register-greeting.blade.php
@@ -3,6 +3,14 @@
 
 @section('content')
   <div class="container">
+    <h2>Registrierung</h2>
+
+    <ol class="breadcrumbs">
+      <li class="active">1. Begrüßung</li>
+      <li>2. Zusage</li>
+      <li>3. Daten</li>
+    </ol>
+
     <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
 
     <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
