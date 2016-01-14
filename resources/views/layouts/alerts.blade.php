ea9c98953a57986658a102fc2af29557f84dd475
Marco Brack <PapstDonB@Googlemail.com>
2016-01-14 15:13:35 +0100

    Center alerts

--- resources/views/layouts/alerts.blade.php
+++ resources/views/layouts/alerts.blade.php
@@ -1,12 +1,12 @@
 <div class="container alerts-container">
   @foreach (Alert::getMessages() as $type => $messages)
-      @foreach ($messages as $message)
-          <div class="alert alert-dismissible alert-{{$type}}" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-            {{$message}}
-          </div>
-      @endforeach
+    @foreach ($messages as $message)
+      <div class="alert alert-dismissible alert-{{$type}}" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        {{$message}}
+      </div>
+    @endforeach
   @endforeach
 </div>
