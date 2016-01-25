abbce752858124f10dd2cc3982b0ad8f3c7aaaa9
Sascha Zimmermann <zimsa@uni-koblenz.de>
2016-01-25 19:07:01 +0100

    add mail template

---
+++ resources/views/emails/no_more_time_slots.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <p>Sehr geehrter {{ $admin_name }},</p>
+        <p>bei einem Aufruf der Registrierung wurde festgestellt, dass der gewünschte Tag - {{ $day }} - bereits belegt ist.</p>
+        <p>Bitte erhöhen Sie bei Bedarf die obere Grenze für die Registrierung.</p>
+        <p>Viele Grüße,<br/>
+            Ihr System GSA Online Plus</p>
+    </body>
+</html>
