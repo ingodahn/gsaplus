f4bdeaad316981b6e90d6fd6847c23d407d9d015
Sascha Zimmermann <zimsa@uni-koblenz.de>
2016-01-25 14:20:34 +0100

    add libraries and files for translation

---
+++ resources/lang/de/regitration.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Registration Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'no_more_time_slots'   => 'Registrierung nicht möglich - alle verfügbaren Plätze sind bereits belegt'
+
+];
