612e75b98263b52085d0dc0c89e3dc0c9ecb9f63
Marco Brack <PapstDonB@Googlemail.com>
2016-01-26 14:08:11 +0100

    Hide registration start if no days are available

--- resources/views/gate/start_page.blade.php
+++ resources/views/gate/start_page.blade.php
@@ -21,6 +21,7 @@
     <div class="row vdivide">
       <div class="col-md-4">
         <h2>Registrierung</h2>
+        @if ($RegistrationPossible)
         <form action="/StartRegistration" method="post">
           <div class="form-group">
             <label for="Code">Persönlicher Code</label>
@@ -30,6 +31,12 @@
           </div>
           <button type="submit" class="btn btn-primary">zur Registrierung</button>
         </form>
+        @else
+          <p>
+            Zur Zeit sind keine Plätze frei, yadda yadda. Sie können jedoch das Team kontaktieren, bla bla.
+          </p>
+          <a href="/ContactTeam" class="btn btn-warning">Team kontaktieren</a>
+        @endif
       </div>
       <div class="col-md-8">
         <h2>Login</h2>
