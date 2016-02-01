9e4d80f3ad8129ce4ab03cb24b91f1d1ecf13cac
Marco Brack <PapstDonB@Googlemail.com>
2016-02-01 11:26:53 +0100

    Prefill day-fields in patient_list with current data

--- resources/views/therapist/patient_list.blade.php
+++ resources/views/therapist/patient_list.blade.php
@@ -11,31 +11,31 @@
         <div class="col-md-2">
           <div class="input-group">
             <span class="input-group-addon">So</span>
-            <input name="So_slots" type="number" class="form-control" value="1">
+            <input name="So_slots" type="number" class="form-control" value="{{$Slots['Sonntag']}}">
           </div>
         </div>
         <div class="col-md-2">
           <div class="input-group">
             <span class="input-group-addon">Mo</span>
-            <input name="Mo_slots"  type="number" class="form-control" value="1">
+            <input name="Mo_slots"  type="number" class="form-control" value="{{$Slots['Montag']}}">
           </div>
         </div>
         <div class="col-md-2">
           <div class="input-group">
             <span class="input-group-addon">Di</span>
-            <input name="Di_slots"  type="number" class="form-control" value="1">
+            <input name="Di_slots"  type="number" class="form-control" value="{{$Slots['Dienstag']}}">
           </div>
         </div>
         <div class="col-md-2">
           <div class="input-group">
             <span class="input-group-addon">Mi</span>
-            <input name="Mi_slots"  type="number" class="form-control" value="1">
+            <input name="Mi_slots"  type="number" class="form-control" value="{{$Slots['Mittwoch']}}">
           </div>
         </div>
         <div class="col-md-2">
           <div class="input-group">
             <span class="input-group-addon">Do</span>
-            <input name="Do_slots"  type="number" class="form-control" value="1">
+            <input name="Do_slots"  type="number" class="form-control" value="{{$Slots['Donnerstag']}}">
           </div>
         </div>
         <div class="col-md-2">
