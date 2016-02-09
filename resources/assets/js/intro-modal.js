8e38165e84f1188825a33dc5023ba44acb250b09
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-02-09 14:47:34 +0100

    Change http to https in YouTube embed
    Fixes #4

--- resources/assets/js/intro-modal.js
+++ resources/assets/js/intro-modal.js
@@ -1,5 +1,5 @@
 function openModal() {
-  var src = 'http://www.youtube.com/embed/LS-VPyLaJFM?html5=1&autoplay=1';
+  var src = 'https://www.youtube.com/embed/LS-VPyLaJFM?html5=1&autoplay=1';
   $('#intro-modal').modal('show');
   $('#intro-modal iframe').attr('src', src);
 }
