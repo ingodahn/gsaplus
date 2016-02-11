df2b8b9e2a920e6f61a3400a2dcff247c984d38f
Sascha Zimmermann <zimsa@uni-koblenz.de>
2016-02-11 15:23:21 +0100

    add patient status to model / factory method

--- database/migrations/2016_01_12_204429_create_patients_table.php
+++ database/migrations/2016_01_12_204429_create_patients_table.php
@@ -18,6 +18,8 @@ class CreatePatientsTable extends Migration
             $table->string('code');
             $table->tinyInteger('assignment_day');
             $table->tinyInteger('assignment_day_changes_left');
+            $table->enum('patient_status', ['P010', 'P020', 'P025', 'P030', 'P040', 'P045', 'P050', 'P060',
+                'P065', 'P070', 'P075', 'P080', 'P090', 'P095', 'P100', 'P110', 'P115', 'P120', 'P130', 'P140']);
 
             $table->integer('therapist_id')->unsigned();
             $table->integer('assignment_id')->unsigned();
