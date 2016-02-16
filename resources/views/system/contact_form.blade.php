7b52aa26ff79981d4e4d2c99b75b0e7b7fb3e15a
Marco Brack <PapstDonB@Googlemail.com>
2016-02-16 16:55:34 +0100

    Implement contact form

--- resources/views/system/contact_form.blade.php
+++ resources/views/system/contact_form.blade.php
@@ -6,5 +6,25 @@
 
         <h2>Kontaktformular</h2>
 
+        <p>
+          Nutzen sie dieses Formular um eine E-Mail an <a href="mailto:PapstDonB@Googlemail.com">PapstDonB@Googlemail.com</a> zu senden.
+        </p>
+
+        <form action="/SendMessage" method="post" enctype="text/plain">
+          <div class="form-group">
+            <label for="name" class="control-label">Name</label>
+            <input name="name" type="text" placeholder="Dr. XTC">
+          </div>
+          <div class="form-group">
+            <label for="mail" class="control-label">E-Mail</label>
+            <input name="mail" type="text" placeholder="dr_xtc@drogen24.onion">
+          </div>
+          <div class="form-group">
+            <label for="text" class="control-label">Nachricht</label>
+            <input name="text" type="text" placeholder="Sehr geehrter Dr Ogen..." size="50"><br><br>
+          </div>
+          <button type="submit" class="btn btn-primary pull-right">Absenden</button>
+        </form>
+
     </div>
 @endsection
