401067b2da1e0c975fbab4fcce8b150b1521f092
Sascha Zimmermann <zimsa@uni-koblenz.de>
2016-02-17 21:22:00 +0100

    add column 'is_random' to different tables

---
+++ database/migrations/2016_02_17_200307_add_random_attribute.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * for testing - TODO: remove when project is finished
+ */
+class AddRandomAttribute extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach (['users', 'assignments', 'assignment_templates', 'responses'] as $table_name) {
+            Schema::table($table_name, function($table) {
+                $table->boolean('is_random');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        foreach (['users', 'assignments', 'assignment_templates', 'responses'] as $table_name) {
+            Schema::table($table_name, function($table) {
+                $table->dropColumn('is_random');
+            });
+        }
+    }
+}
