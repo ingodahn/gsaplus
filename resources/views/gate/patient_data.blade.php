722ee16e8dc087bd246dd0767cbcb952fbd2bcfa
Marco Brack <PapstDonB@Googlemail.com>
2016-02-18 17:08:48 +0100

    Add hint to blogday select. Closes issue #23.

--- resources/views/gate/patient_data.blade.php
+++ resources/views/gate/patient_data.blade.php
@@ -28,6 +28,9 @@
 
       <div class="form-group">
         <label for="day_of_week" class="control-label">Wochentag</label>
+        <a href="javascript:void(0)" data-toggle="popover" data-trigger="focus" title="Warum sind nicht alle Wochentage wählbar?" data-content="Wir möchten, dass Sie nach dem Schreiben Ihres Blog möglichst innerhalb von 24 h eine Rückmeldung Ihres Online-Therapeuten erhalten. Da wir dies jedoch nur von Montag bis Freitag mit begrenzten Kapazitäten zusagen können, sind nicht alle Tage als Schreibtage wählbar.">
+          <i class="fa fa-question-circle"></i>
+        </a>
         <select name="day_of_week" class="form-control" required>
           @@foreach($DayOfWeek as $available_day)
             <option>{{$available_day}}</option>
