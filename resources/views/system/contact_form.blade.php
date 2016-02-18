689a567a9658f245c7ba3511e0bc53f996413a6f
Marco Brack <PapstDonB@Googlemail.com>
2016-02-18 16:35:36 +0100

    Fix the contact form layout

--- resources/views/system/contact_form.blade.php
+++ resources/views/system/contact_form.blade.php
@@ -10,19 +10,19 @@
           Nutzen sie dieses Formular um eine E-Mail an <a href="mailto:PapstDonB@Googlemail.com">PapstDonB@Googlemail.com</a> zu senden.
         </p>
 
-        <form action="/SendMessage" method="post" enctype="text/plain">
+        <form action="/SendMessage" method="post">
           {{ csrf_field() }}
           <div class="form-group">
             <label for="name" class="control-label">Name</label>
-            <input name="name" type="text" placeholder="Dr. XTC">
+            <input name="name" type="text" class="form-control" placeholder="Dr. XTC"></input>
           </div>
           <div class="form-group">
             <label for="mail" class="control-label">E-Mail</label>
-            <input name="mail" type="text" placeholder="dr_xtc@drogen24.onion">
+            <input name="mail" type="text" class="form-control" placeholder="dr_xtc@drogen24.onion"></input>
           </div>
           <div class="form-group">
             <label for="text" class="control-label">Nachricht</label>
-            <input name="text" type="text" placeholder="Sehr geehrter Dr Ogen..." size="50"><br><br>
+            <textarea name="text" type="textarea" rows="5" class="form-control" placeholder="Sehr geehrter Dr Ogen..."></textarea>
           </div>
           <button type="submit" class="btn btn-primary pull-right">Absenden</button>
         </form>
