f260e9441e7522ac2f5260b4c0784911c4623761
Sascha Zimmermann <zimsa@uni-koblenz.de>
2016-02-18 18:27:07 +0100

    add script for rake

---
+++ database/sql/drop_all_tables.sql
@@ -0,0 +1,8 @@
+DROP TABLE migrations;
+DROP TABLE users;
+DROP TABLE password_resets;
+DROP TABLE assignment_templates;
+DROP TABLE assignments;
+DROP TABLE responses;
+DROP TABLE week_days;
+DROP TABLE codes;
