40aee89f8a549956f7d52bb60e8545d31e07cb6b
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-03-04 14:03:35 +0100

    Change Contact Form placeholders to a more harmless version

--- resources/views/system/contact_form.blade.php
+++ resources/views/system/contact_form.blade.php
@@ -7,22 +7,22 @@
         <h2>Kontaktformular</h2>
 
         <p>
-          Nutzen sie dieses Formular um eine E-Mail an <a href="mailto:PapstDonB@Googlemail.com">PapstDonB@Googlemail.com</a> zu senden.
+          Nutzen sie dieses Formular um eine E-Mail an <a href="mailto:kontakt@gsa-online-plus.net">kontakt@gsa-online-plus.net</a> zu senden.
         </p>
 
         <form action="/SendMessage" method="post">
           {{ csrf_field() }}
           <div class="form-group">
             <label for="eMail" class="control-label">E-Mail</label>
-            <input name="eMail" type="text" class="form-control" placeholder="dr_xtc@drogen24.onion"></input>
+            <input name="eMail" type="text" class="form-control" placeholder="rapunzel@turm.de"></input>
           </div>
           <div class="form-group">
             <label for="subject" class="control-label">Betreff</label>
-            <input name="subject" type="text" class="form-control" placeholder="Wadde hadde dudeda?"></input>
+            <input name="subject" type="text" class="form-control" placeholder="Problem und mögliche Lösung"></input>
           </div>
           <div class="form-group">
             <label for="message" class="control-label">Nachricht</label>
-            <textarea name="message" rows="5" class="form-control" placeholder="Sehr geehrter Dr Ogen..."></textarea>
+            <textarea name="message" rows="5" class="form-control" placeholder="Ich will gerne mit dir gehen, aber ich weiß nicht, wie ich herabkommen kann. Wenn du kommst, so bring jedesmal einen Strang Seide mit, daraus will ich eine Leiter flechten, und wenn die fertig ist, so steige ich herunter, und du nimmst mich auf dein Pferd."></textarea>
           </div>
           <button type="submit" class="btn btn-primary pull-right">Absenden</button>
           <a class="btn btn-primary pull-right" style="margin-right: 10px;" href="/Home">Zurück</a>
