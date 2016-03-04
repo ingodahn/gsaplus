3d29f45712c5c168c0b1804db4f7914a36d70a8d
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-03-04 14:48:38 +0100

    Turn off autocomplete for password during registration
    This is related to Issue #35
    Please test

--- resources/views/gate/patient_data.blade.php
+++ resources/views/gate/patient_data.blade.php
@@ -73,12 +73,12 @@
       <div class="row">
         <div class="form-group col-sm-6">
           <label for="password" class="control-label">Passwort</label>
-          <input name="password" id="password" type="password" class="form-control width-100" placeholder="hunter2 (mindestens 6 Zeichen)" required minlength="6">
+          <input name="password" id="password" type="password" autocomplete="off" class="form-control width-100" placeholder="hunter2 (mindestens 6 Zeichen)" required minlength="6">
         </div>
 
         <div class="form-group col-sm-6">
           <label class="control-label">Wiederholen</label>
-          <input type="password" class="form-control width-100" placeholder="hunter2" required minlength="6" data-parsley-equalto="#password">
+          <input type="password" autocomplete="off" class="form-control width-100" placeholder="hunter2" required minlength="6" data-parsley-equalto="#password">
         </div>
       </div>
 
