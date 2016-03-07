12d5a1cbd9cee4d21549efd57af870b82526537f
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-03-07 15:37:12 +0100

    Tweak the layout of the start page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-default" role="navigation">
   <div class="container">
-    <a href="/"><img src="/img/logo.svg" alt="GSA Online Plus Logo" title="zur Startseite" class="nav-logo"/></a>
+    <a href="/" ><img src="/img/logo.svg" alt="GSA Online Plus Logo" title="zur Startseite" class="nav-logo"/></a>
   </div>
 </nav>
