7f5133a29f9f24652553f3167154d6df77404eca
Sascha Zimmermann <zimsa@uni-koblenz.de>
2016-03-10 17:17:03 +0100

    fix typo

--- resources/views/patient/patient_profile.blade.php
+++ resources/views/patient/patient_profile.blade.php
@@ -50,7 +50,7 @@
     <hr>
     <h3>Tagebuchtag</h3>
     <p>
-      Der aktuelle Tagebuchtag ist <strong>{{ $Patient['assignment_day'] }}</strong> und es verbleiben noch <strong>{{ $Patient['assignmentDayChagesLeft'] }} Änderungen</strong>.
+      Der aktuelle Tagebuchtag ist <strong>{{ $Patient['assignment_day'] }}</strong> und es verbleiben noch <strong>{{ $Patient['assignmentDayChangesLeft'] }} Änderungen</strong>.
     </p>
     <form data-parsley-validate role="form" action="/patient/{{$Name}}/day_of_week" method="post">
       {{ csrf_field() }}
