6c9affcbcee81e6dba76097331f0952ee07f4377
turbopope <PapstDonB@Googlemail.com>
2016-03-14 22:43:38 +0100

    Default to info alert if no type is given

--- resources/views/layouts/alerts.blade.php
+++ resources/views/layouts/alerts.blade.php
@@ -1,6 +1,7 @@
 <div class="container alerts-container">
   @foreach (Alert::getMessages() as $type => $messages)
     @foreach ($messages as $message)
+      <?php if($type == 0) $type = "info" ?>
       <div class="alert alert-dismissible alert-{{$type}}" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
