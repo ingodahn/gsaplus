bbbe0bb2e9532ae310320bb5fdcbea12f0cbd239
turbopope <PapstDonB@Googlemail.com>
2016-03-14 21:47:53 +0100

    Disable state saving in patient table (closes #48)

--- resources/views/therapist/patient_list.blade.php
+++ resources/views/therapist/patient_list.blade.php
@@ -18,7 +18,6 @@
         "language": {
           "url": "/js/dataTable-german.json"
         },
-        stateSave: true,
         ajax: '{!! route('datatables.data') !!}',
         columns: [
           { data: 'selection', name: 'selection', orderable: false, searchable: false},
