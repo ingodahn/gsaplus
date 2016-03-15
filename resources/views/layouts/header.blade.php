c4725636ea143b6b09e6ad95375c6ac2d63481a0
turbopope <PapstDonB@Googlemail.com>
2016-03-15 13:11:23 +0100

    Add real links to navbar (WIP)

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,20 +14,38 @@
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link 2</a></li>
-        <li><a href="#">Link 3</a></li>
-        <li><a href="#">Link 4</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
+
+        @if($isLoggedIn)
+
+          {{-- Patient --}}
+          @if($isPatient)
+            <li><a href="/Home">Mein Tagebuch</a></li>
+            <li><a href="/Profile/{{$Name}}">Mein Profil</a></li>
+          @endif
+
+
+          {{-- Therapist --}}
+          @if($isTherapist)
+            <li><a href="/Home">Patientenliste</a></li>
+            <li><a href="/AdminUsers">AdminUsers</a></li>
+            <li><a href="/AdminCodes">AdminCodes</a></li>
+          @endif
+
+
+          {{-- Common --}}
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{$Name}} <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              {{-- <li><a href="#">Action</a></li> --}}
+              {{-- <li><a href="#">Another action</a></li> --}}
+              {{-- <li><a href="#">Something else here</a></li> --}}
+              {{-- <li role="separator" class="divider"></li> --}}
+              <li><a href="/Logout">Ausloggen</a></li>
+            </ul>
+          </li>
+
+        @endif
+
       </ul>
     </div>
 
