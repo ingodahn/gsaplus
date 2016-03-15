f72a1bd454afc9116634c10cf87c5d62ff16ab14
Sascha Zimmermann <zimsa@uni-koblenz.de>
2016-03-15 19:02:52 +0100

    add locale files (needed to localize dates)

--- provision.sh
+++ provision.sh
@@ -5,3 +5,5 @@ apt-get install -y fish tree
 wget -qNO /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
 echo "Installed $(phpunit --version)"
+
+sudo apt-get install language-pack-DE -y
