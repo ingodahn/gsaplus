163f4d5beb93dcaa5fb7afb77aca066fe0d5b4ce
turbopope <PapstDonB@Googlemail.com>
2016-03-29 12:53:13 +0200

    Style the diary overview

--- resources/views/patient/diary.blade.php
+++ resources/views/patient/diary.blade.php
@@ -8,20 +8,35 @@
     <p>
       Dies ist das Tagebuch von <em>{{ $Diary['name'] }}</em>. Es enthält eine Übersicht aller geplanten und geschriebenen Einträge mit ihrem jeweiligen Status.
     </p>
-	<p>Es ist Woche {{ $Diary['patient_week'] }}/12</p>
-	<p>
-      <a href="/Profile/{{ $Diary['name'] }}" class="btn btn-warning">Profil</a>
-    </p>
+
+    <p>Es ist Woche <strong>{{$Diary['patient_week']}}</strong> von 12.</p>
 
     <p>
-      <ul>
-        @for ($i=1; $i <= 12; $i++)
-        <li> Woche {{ $i }}: {{ $Diary['entries'][$i]['problem'] }} ({{ $Diary['entries'][$i]['entry_status'] }})
-          <a href="/Assignment/{{ $Diary['name'] }}/{{ $i }}" class="btn btn-warning">Ansehen</a>
-        </li>
-        @endfor
-      </ul>
+      <a href="/Profile/{{$Diary['name']}}">Profil von {{$Diary['name']}}.</a>
     </p>
+
+    <table class="table table-striped table-bordered table-condensed">
+      <thead>
+        <th>Woche</th>
+        <th>Aufgabe</th>
+        <th>Status</th>
+        <th>Aktionen</th>
+      </thead>
+      <tbody>
+      @for ($i=1; $i <= 12; $i++)
+        <tr>
+          <td>{{$i}}</td>
+          <td>{{$Diary['entries'][$i]['problem']}}</td>
+          <td>
+            <code>{{$Diary['entries'][$i]['entry_status']}}</code>
+          </td>
+          <td>
+            <a href="/Assignment/{{$Diary['name']}}/{{$i}}">Ansehen</a>
+          </td>
+        </tr>
+      @endfor
+      </tbody>
+    </table>
 
   </div>
 @endsection
