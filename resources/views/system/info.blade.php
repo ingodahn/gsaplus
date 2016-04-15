7863a4d9d7074546da963266e1566987283891a3
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-04-15 12:48:02 +0200

    Add proper text to the info page

--- resources/views/system/info.blade.php
+++ resources/views/system/info.blade.php
@@ -24,42 +24,46 @@
   </script>
 
   <div class="container">
-    <h2>Experteninformationen</h2>
+    <h2>Hintergrundinformationen zum GSA online plus</h2>
+
+    <p>
+      In Form von kurzen Videos erhalten Sie auf dieser Seite Hintergrundinformationen zum GSA online plus.
+    </p>
 
     <div id="toc" class="list-group">
       <a href="#video-1" class="list-group-item">
-        <h4 class="list-group-item-heading">Some video</h4>
-        <p class="list-group-item-text">Sehen sie hier, wie ein Mann lacht.</p>
+        <h4 class="list-group-item-heading">Wer sind wir?</h4>
+        <p class="list-group-item-text">Begrüßung und Angebot der online Nachsorge</p>
       </a>
       <a href="#video-2" class="list-group-item">
-        <h4 class="list-group-item-heading">Another video</h4>
-        <p class="list-group-item-text">Sehen sie hier, wie ein Mann weint.</p>
+        <h4 class="list-group-item-heading">Rückkehr an den Arbeitsplatz</h4>
+        <p class="list-group-item-text">Warum onlinebasierte Unterstützung bei der Wiedereingliederung?</p>
       </a>
       <a href="#video-3" class="list-group-item">
-        <h4 class="list-group-item-heading">Awesome video</h4>
-        <p class="list-group-item-text">Ein Mann hat viele Gesichter.</p>
+        <h4 class="list-group-item-heading">Ablauf und Durchführung</h4>
+        <p class="list-group-item-text">Wie läuft die online Nachsorge ab?</p>
       </a>
       <a href="#video-4" class="list-group-item">
-        <h4 class="list-group-item-heading">Boring video</h4>
-        <p class="list-group-item-text">Ein Mann bittet ein Mädchen um Verzeihung.</p>
+        <h4 class="list-group-item-heading">Die ZBKT Methode</h4>
+        <p class="list-group-item-text">Welche Grundlage hat die online Nachsorge?</p>
       </a>
       <a href="#video-5" class="list-group-item">
-        <h4 class="list-group-item-heading">Video about videos</h4>
-        <p class="list-group-item-text">Ein Mädchen spielt mit dem Feuer. Ein Mädchen weiß nicht, was gut für es ist. Ein Mädchen sollte weg rennen.</p>
+        <h4 class="list-group-item-heading">Ein Beispiel</h4>
+        <p class="list-group-item-text">Eine ganz normale Wiedereingliederung</p>
       </a>
       <a href="#video-6" class="list-group-item">
-        <h4 class="list-group-item-heading">Videooooooo</h4>
-        <p class="list-group-item-text">Ein Mann tippt Wörter auf einer Tastatur.</p>
+        <h4 class="list-group-item-heading">Am Ball Bleiben</h4>
+        <p class="list-group-item-text">Warum ist es wichtig dabei zu bleiben?</p>
       </a>
     </div>
 
     <div class="info-video" id="video-1">
-      <h3>Some video</h3>
+      <h3>Wer sind wir?</h3>
       <div class="pull-right">
         <a href="#toc">Nach oben</a>
       </div>
       <p>
-        Sehen sie hier, wie ein Mann lacht.
+        Begrüßung und Angebot der online Nachsorge
       </p>
       <div class="videoWrapper">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/RvOnXh3NN9w" frameborder="0" allowfullscreen></iframe>
@@ -67,12 +71,12 @@
     </div>
 
     <div class="info-video" id="video-2">
-      <h3>Another video</h3>
+      <h3>Rückkehr an den Arbeitsplatz</h3>
       <div class="pull-right">
         <a href="#toc">Nach oben</a>
       </div>
       <p>
-        Sehen sie hier, wie ein Mann weint.
+        Warum onlinebasierte Unterstützung bei der Wiedereingliederung?
       </p>
       <div class="videoWrapper">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/ao8L-0nSYzg" frameborder="0" allowfullscreen></iframe>
@@ -80,12 +84,12 @@
     </div>
 
     <div class="info-video" id="video-3">
-      <h3>Awesome video</h3>
+      <h3>Ablauf und Durchführung</h3>
       <div class="pull-right">
         <a href="#toc">Nach oben</a>
       </div>
       <p>
-        Ein Mann hat viele Gesichter.
+        Wie läuft die online Nachsorge ab?
       </p>
       <div class="videoWrapper">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/sNhhvQGsMEc" frameborder="0" allowfullscreen></iframe>
@@ -93,12 +97,12 @@
     </div>
 
     <div class="info-video" id="video-4">
-      <h3>Boring video</h3>
+      <h3>Die ZBKT Methode</h3>
       <div class="pull-right">
         <a href="#toc">Nach oben</a>
       </div>
       <p>
-        Ein Mann bittet ein Mädchen um Verzeihung.
+        Welche Grundlage hat die online Nachsorge?
       </p>
       <div class="videoWrapper">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/QOCaacO8wus" frameborder="0" allowfullscreen></iframe>
@@ -106,12 +110,12 @@
     </div>
 
     <div class="info-video" id="video-5">
-      <h3>Video about videos</h3>
+      <h3>Ein Beispiel</h3>
       <div class="pull-right">
         <a href="#toc">Nach oben</a>
       </div>
       <p>
-        Ein Mädchen spielt mit dem Feuer. Ein Mädchen weiß nicht, was gut für es ist. Ein Mädchen sollte weg rennen.
+        Eine ganz normale Wiedereingliederung
       </p>
       <div class="videoWrapper">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/4_aOIA-vyBo" frameborder="0" allowfullscreen></iframe>
@@ -119,12 +123,12 @@
     </div>
 
     <div class="info-video" id="video-6">
-      <h3>Videooooooo</h3>
+      <h3>Am Ball Bleiben</h3>
       <div class="pull-right">
         <a href="#toc">Nach oben</a>
       </div>
       <p>
-        Ein Mann tippt Wörter auf einer Tastatur.
+        Warum ist es wichtig dabei zu bleiben?
       </p>
       <div class="videoWrapper">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/V9_PjdU3Mpo" frameborder="0" allowfullscreen></iframe>
