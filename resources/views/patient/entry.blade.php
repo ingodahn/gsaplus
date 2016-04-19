5993277db2844daab42eddf7499acd88ae4d967b
Dr. Bernd Ingo Dahn <dahn@uni-koblenz.de>
2016-04-19 21:32:44 +0200

    Revert "change visibility of problem / situations"
    This reverts commit 673f7188ab58174bb0ef03a1a5b0785a869d526c

--- resources/views/patient/entry.blade.php
+++ resources/views/patient/entry.blade.php
@@ -17,25 +17,20 @@
         </div>
       @endif
 
-
       <h3>Fragestellung</h3>
-      @if($EntryInfo['week'] > 1 )
-        @if($isPatient || $isTherapist && $EntryInfo['status'] >= 'E020')
-          <p>{{$EntryInfo['problem']}}</p>
-        @elseif($isTherapist && $EntryInfo['week'] > 1 && $EntryInfo['status'] < 'E020')
-          <p>
-          <div class="form-group">
-            <label for="problem">Problem bearbeiten</label>
-            <textarea class="form-control js-auto-size" name="problem">{{$EntryInfo['problem']}}</textarea>
-          </div>
-        </p>
-        @endif
-      @else
-        <p><em>Bitte beschreiben Sie verschiedene Situationen bezüglich der Rückkehr an Ihren Arbeitsplatz...</em></p>
+      @if($isPatient || $isTherapist && $EntryInfo['status'] >= 'E020')
+        <p>{{$EntryInfo['problem']}}</p>
+      @elseif($isTherapist && $EntryInfo['status'] < 'E020')
+        <p>
+        <div class="form-group">
+          <label for="problem">Problem bearbeiten</label>
+          <textarea class="form-control js-auto-size" name="problem">{{$EntryInfo['problem']}}</textarea>
+        </div>
+      </p>
       @endif
 
       <?php
-        $visible = $isPatient || $isTherapist &&  $isTherapist && $EntryInfo['week'] <= 1;
+        $visible = $isPatient || $isTherapist &&  $isTherapist && $EntryInfo['status'] >= 'E040';
         $editable = $isPatient && $EntryInfo['status'] < 'E040';
       ?>
       @if($visible)
