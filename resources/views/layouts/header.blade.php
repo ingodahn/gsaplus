5783fbda0717213a5a3d9acd11bc1676f477f075
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-04-25 13:44:22 +0200

    Set target to blank for all admin tools

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -30,9 +30,9 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin-Tools <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="/test">Konrollzentrum</a></li>
-                <li><a href="/AdminUsers">Alle Nutzer</a></li>
-                <li><a href="/AdminCodes">Alle Codes</a></li>
+                <li><a href="/test" target="_blank">Konrollzentrum</a></li>
+                <li><a href="/AdminUsers" target="_blank">Alle Nutzer</a></li>
+                <li><a href="/AdminCodes" target="_blank">Alle Codes</a></li>
               </ul>
             </li>
           @endif
