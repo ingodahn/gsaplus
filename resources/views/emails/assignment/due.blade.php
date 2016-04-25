6742ec5b01735b07f3fdc3501ef3db7a3fe53b46
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-04-25 13:58:49 +0200

    Revert "Correct wording"
    This reverts commit d2c9de38a72fb777ce91e251ab8c5ca37d7b46b5.

--- resources/views/emails/assignment/due.blade.php
+++ resources/views/emails/assignment/due.blade.php
@@ -3,7 +3,7 @@
 <p>Sehr geehrter Patient,</p>
 <p>wir wollen Sie darüber informieren, dass Sie Ihren letzten Schreibimpuls noch nicht bearbeitet haben.</p>
 <p>Sie können den Tagebucheintrag bis zum nächsten Schreibtag schreiben. Wir werden uns bemühen, Ihren
-    Eintrag einzusehen und ihnen Rückmeldung zu geben.</p>
+    Eintrag einzusehen und zu kommentieren.</p>
 <p>Bitte besuchen Sie unsere Webeiste um sich den letzten Schreibimpuls anzusehen und einen Tagebucheintrag zu verfassen.</p>
 <p>Viele Grüße,<br/>
     Ihr Team GSA Online Plus</p>
