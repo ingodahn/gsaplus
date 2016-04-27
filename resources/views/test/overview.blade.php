ef151433d849941d15bf438183d98ff70607a036
Sascha Zimmermann <zimsa@uni-koblenz.de>
2016-04-28 01:44:00 +0200

    add setting to test overview / organize view in rows

--- resources/views/test/overview.blade.php
+++ resources/views/test/overview.blade.php
@@ -36,32 +36,36 @@
     <?php use App\Models\UserRole; ?>
 
     <div class="container" id="top">
-        <h3>Softwaretest-Kontrollzentrum</h3>
-        <p>Auf dieser Seite können sie die im System vorhandenen</p>
-        <ul>
-            <li>
-                <a href="#patients">Patienten</a>
-            </li>
-            <li>
-                <a href="#therapists">Therapeuten</a>, und
-            </li>
-            <li>
-                <a href="#admins">Administratoren</a>
-            </li>
-        </ul>
-        <p>einsehen.</p>
-        <p>Es ist möglich sich im Namen eines bestimmten Benutzers einzuloggen, die Mails eines
-            Benutzers einzusehen und das Datum auf ein in der Zukunft liegendes Schreibdatum zu ändern
-            (falls der Benutzer ein Patient ist).</p>
-        <p>Patienten können <a href="#reminders">daran erinnert</a> werden,
-            dass Sie den ersten bzw. einen Folge-Schreibimpuls erhalten oder den aktuellen Schreibimpuls 5 Tage lang
-            nicht bearbeitet haben.</p>
-        <p>In den <a href="#config">Einstellungen</a> kann das aktuelle Test-Datum gewählt werden. Zudem
-            können bestimmte Erinnerungen automatisch verschickt werden, wenn sich das Test-Datum ändert.</p>
 
-        <hr/>
+        <div class="row">
+            <h3>Softwaretest-Kontrollzentrum</h3>
+            <p>Auf dieser Seite können sie die im System vorhandenen</p>
+            <ul>
+                <li>
+                    <a href="#patients">Patienten</a>
+                </li>
+                <li>
+                    <a href="#therapists">Therapeuten</a>, und
+                </li>
+                <li>
+                    <a href="#admins">Administratoren</a>
+                </li>
+            </ul>
+            <p>einsehen.</p>
+            <p>Es ist möglich sich im Namen eines bestimmten Benutzers einzuloggen, die Mails eines
+                Benutzers einzusehen und das Datum auf ein in der Zukunft liegendes Schreibdatum zu ändern
+                (falls der Benutzer ein Patient ist).</p>
+            <p>Patienten können <a href="#reminders">daran erinnert</a> werden,
+                dass Sie den ersten bzw. einen Folge-Schreibimpuls erhalten oder den aktuellen Schreibimpuls 5 Tage lang
+                nicht bearbeitet haben.</p>
+            <p>In den <a href="#config">Einstellungen</a> kann das aktuelle Test-Datum gewählt werden. Zudem
+                können bestimmte Erinnerungen automatisch verschickt werden, wenn sich das Test-Datum ändert.</p>
+
+            <hr/>
+        </div>
 
         @foreach($infos as $role => $users)
+        <div class="row">
             <h4 id="{{ $role }}s">
             @if($role == UserRole::PATIENT)
                 Patienten
@@ -135,126 +139,139 @@
                     <span class="glyphicon glyphicon-arrow-up"></span>
                 </a>
             </p>
+        </div>
         @endforeach
 
-        <h4 id="reminders">Erinnerungen</h4>
+        <div class="row">
+            <h4 id="reminders">Erinnerungen</h4>
 
-        <table class="table table-striped table-bordered table-hover">
-            <thead>
-                <tr>
-                    <th>Ereignis</th>
-                    <th>Erklärung</th>
-                    <th>Aktion</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Erhalt des ersten Schreibimpulses</td>
-                    <td>
-                        Gilt für alle Patienten die heute (bzw. zum Testdatum) Ihren ersten Schreibtag haben.
-                    </td>
-                    <td>
-                        <form method="POST" action="/test/send-reminders/first">
-                            {{ csrf_field() }}
-                            <input class="btn-link" value="Benachrichtigungen versenden" type="submit" />
-                        </form>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Erhalt eines Folge-Schreibimpulses</td>
-                    <td>
-                        Gilt für alle Patienten die heute (bzw. zum Testdatum) Ihren neuen Schreibtag haben.
-                    </td>
-                    <td>
-                        <form method="POST" action="/test/send-reminders/new">
-                            {{ csrf_field() }}
-                            <input class="btn-link" value="Benachrichtigungen versenden" type="submit" />
-                        </form>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Aktueller Schreibimpuls unbearbeitet</td>
-                    <td>
-                        Gilt für alle Patienten die Ihren aktuellen Schreibimpuls 5 Tage lang nicht bearbeitet haben.
-                    </td>
-                    <td>
-                        <form method="POST" action="/test/send-reminders/due">
-                            {{ csrf_field() }}
-                            <input class="btn-link" value="Benachrichtigungen versenden" type="submit" />
-                        </form>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-
-        <p>Wenn ein Patient an einen aktuellen Schreibimpuls erinnert wird, so wird ebenso das <em>nächste
-            Schreibdatum (+1 Woche)</em> berechnet.</p>
-
-        <br/>
-
-        <h4 id="config">Einstellungen</h4>
-
-        <form role="form" action="/test/settings" method="post">
-            {{ csrf_field() }}
             <table class="table table-striped table-bordered table-hover">
                 <thead>
-                <tr>
-                    <th>Einstellung</th>
-                    <th>Wert</th>
-                </tr>
+                    <tr>
+                        <th>Ereignis</th>
+                        <th>Erklärung</th>
+                        <th>Aktion</th>
+                    </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td class="col-xs-7">Aktuelles Datum</td>
+                        <td>Erhalt des ersten Schreibimpulses</td>
                         <td>
-                            <div class='input-group date' id='datetimepicker'>
-                                <input name="test_date" type='text' value="{{ $settings['testDate'] }}" class="form-control" required>
-                                        <span class="input-group-addon">
-                                          <span class="glyphicon glyphicon-calendar"></span>
-                                        </span>
-                                </input>
-                            </div>
-                            <script type="text/javascript">
-                                $(function () {
-                                    $('#datetimepicker').datetimepicker({
-                                        locale: 'de',
-                                        format: 'DD.MM.YYYY'
-                                    });
-                                });
-                            </script>
+                            Gilt für alle Patienten die heute (bzw. zum Testdatum) Ihren ersten Schreibtag haben.
+                        </td>
+                        <td>
+                            <form method="POST" action="/test/send-reminders/first">
+                                {{ csrf_field() }}
+                                <input class="btn-link" value="Benachrichtigungen versenden" type="submit" />
+                            </form>
                         </td>
                     </tr>
                     <tr>
+                        <td>Erhalt eines Folge-Schreibimpulses</td>
                         <td>
-                            Automatisch an ersten Schreibimpuls erinnern
+                            Gilt für alle Patienten die heute (bzw. zum Testdatum) Ihren neuen Schreibtag haben.
                         </td>
                         <td>
-                            <input type="checkbox" class="pull-right" name="first_reminder" value="1" {{ $settings['firstReminder'] ? 'checked' : ''}}>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Automatisch an neuen Schreibimpuls erinnern
-                        </td>
-                        <td>
-                            <input type="checkbox" class="pull-right" name="new_reminder" value="1" {{ $settings['newReminder'] ? 'checked' : '' }}>
+                            <form method="POST" action="/test/send-reminders/new">
+                                {{ csrf_field() }}
+                                <input class="btn-link" value="Benachrichtigungen versenden" type="submit" />
+                            </form>
                         </td>
                     </tr>
                     <tr>
+                        <td>Aktueller Schreibimpuls unbearbeitet</td>
                         <td>
-                            Automatisch an Schreibimpuls erinnern, der in Kürze fällig ist
+                            Gilt für alle Patienten die Ihren aktuellen Schreibimpuls 5 Tage lang nicht bearbeitet haben.
                         </td>
                         <td>
-                            <input type="checkbox" class="pull-right" name="due_reminder"  value="1" {{ $settings['dueReminder'] ? 'checked' : ''}}>
+                            <form method="POST" action="/test/send-reminders/due">
+                                {{ csrf_field() }}
+                                <input class="btn-link" value="Benachrichtigungen versenden" type="submit" />
+                            </form>
                         </td>
                     </tr>
                 </tbody>
             </table>
-            <p class="pull-right">
-                <button type="submit" class="btn btn-primary" name="reset_settings">Wiederherstellen</button>
-                <button type="submit" class="btn btn-primary" name="save_settings">Speichern</button>
-            </p>
-        </form>
+        </div>
+
+        <div class="row">
+            <h4 id="config">Einstellungen</h4>
+
+            <form role="form" action="/test/settings" method="post">
+                {{ csrf_field() }}
+                <p>Normalerweise wird der Folgeschreibtag berechnet wenn ein Nutzer über den Erhalt
+                    einer aktuellen Aufgabe informiert wird. Dies kann zusammen mit Datumsverschiebungen zu
+                    Widersprüchen führen. Deshalb ist die Berechnung für Testzwecke ausgeschaltet.
+                    Bitte ändern Sie diese Einstellung nur wenn Sie wissen was Sie tun.</p>
+                <table class="table table-striped table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        <th>Einstellung</th>
+                        <th>Wert</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="col-xs-7">Aktuelles Datum</td>
+                            <td>
+                                <div class='input-group date' id='datetimepicker'>
+                                    <input name="test_date" type='text' value="{{ $settings['testDate'] }}" class="form-control" required>
+                                            <span class="input-group-addon">
+                                              <span class="glyphicon glyphicon-calendar"></span>
+                                            </span>
+                                    </input>
+                                </div>
+                                <script type="text/javascript">
+                                    $(function () {
+                                        $('#datetimepicker').datetimepicker({
+                                            locale: 'de',
+                                            format: 'DD.MM.YYYY'
+                                        });
+                                    });
+                                </script>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Automatisch an ersten Schreibimpuls erinnern
+                            </td>
+                            <td>
+                                <input type="checkbox" class="pull-right" name="first_reminder" value="1" {{ $settings['firstReminder'] ? 'checked' : ''}}>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Automatisch an neuen Schreibimpuls erinnern
+                            </td>
+                            <td>
+                                <input type="checkbox" class="pull-right" name="new_reminder" value="1" {{ $settings['newReminder'] ? 'checked' : '' }}>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Automatisch an Schreibimpuls erinnern, der in Kürze fällig ist
+                            </td>
+                            <td>
+                                <input type="checkbox" class="pull-right" name="due_reminder"  value="1" {{ $settings['dueReminder'] ? 'checked' : ''}}>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Bei einer Erinnerung automatisch den nächsten Schreibtag berechnen
+                            </td>
+                            <td>
+                                <input type="checkbox" class="pull-right" name="calc_next_writing_date"  value="1" {{ $settings['calcNextWritingDate'] ? 'checked' : ''}}>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p class="pull-right">
+                    <button type="submit" class="btn btn-default" name="reset_settings">Einstellungen wiederherstellen</button>
+                    <button type="submit" class="btn btn-primary" name="save_settings">Speichern</button>
+                </p>
+            </form>
+
+        </div>
 
     </div>
 @endsection
