b168d5f34b5171d0075b47bcfa850ce16c3ad84a
Sascha Zimmermann <zimsa@uni-koblenz.de>
2016-04-28 02:38:24 +0200

    make attribute calc_next_writing_date fillable (model for test settings)

--- app/TestSetting.php
+++ app/TestSetting.php
@@ -20,7 +20,8 @@ class TestSetting extends InfoModel
         'test_date',
         'first_reminder',
         'new_reminder',
-        'due_reminder'
+        'due_reminder',
+        'calc_next_writing_date'
     ];
 
     /*
