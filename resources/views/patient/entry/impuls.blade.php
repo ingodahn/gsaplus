29d3ee0f3a1638577835065bfdff8d1a9f04d2f8
dahn <dahn@uni-koblenz.de>
2016-05-07 22:36:50 +0200

    Implemented recall of new templates

--- resources/views/patient/entry/impuls.blade.php
+++ resources/views/patient/entry/impuls.blade.php
@@ -3,52 +3,57 @@
   <p>{!! nl2br(e($EntryInfo['problem'])) !!}</p>
 @elseif($isTherapist && $EntryInfo['status'] < 'E020')
   <p>
-
-    <div class="form-group">
-      <script type="text/javascript">
-        function getProblemTemplate(inhalt)
+    <script type="text/javascript">
+      function getProblemTemplate(inhalt)
+      {
+        if (inhalt=="Kein Muster gewählt")
         {
-          alert(document.getElementById("problem").innerHTMLvalue);
-          if (inhalt=="Kein Muster gewählt")
-          {
-            document.getElementById("problem").innerHTML="";
-            return;
-          }
-          if (window.XMLHttpRequest)
-          {
-            // AJAX nutzen mit IE7+, Chrome, Firefox, Safari, Opera
-            xmlhttp=new XMLHttpRequest();
-          }
-          else
-          {
-            // AJAX mit IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-          }
-          xmlhttp.onreadystatechange=function()
-          {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-              document.getElementById("problem").innerHTML=xmlhttp.responseText;
-            }
-          }
-          xmlhttp.open("GET","/AssignmentTemplate?templateTitle="+inhalt,true);
-          xmlhttp.send();
+          document.getElementById("template_title").value="";
+          document.getElementById("problem").value="";
+          return;
         }
-      </script>
+        if (window.XMLHttpRequest)
+        {
+          // AJAX nutzen mit IE7+, Chrome, Firefox, Safari, Opera
+          xmlhttp=new XMLHttpRequest();
+        }
+        else
+        {
+          // AJAX mit IE6, IE5
+          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function()
+        {
+          if (xmlhttp.readyState==4 && xmlhttp.status==200)
+          {
+            document.getElementById("template_title").value=inhalt;
+            document.getElementById("problem").value=xmlhttp.responseText;
+          }
+        }
+        xmlhttp.open("GET","/AssignmentTemplate?templateTitle="+inhalt,true);
+        xmlhttp.send();
+      }
+    </script>
+
       <p>
-    <label for="choose_template">Muster:</label>
+  <span class="form-group">
+    <label for="template_title">Muster:</label>
     <input type="text" name="template_title" id="template_title" size="50">
+    </span>
+  <span class="form-group">
+    <label for="vhoose_template" class="sr-only">Musterauswahl:</label>
       <select name="choose_template" id="choose_template" onchange=getProblemTemplate(this.value) size="1">
         <option selected>Kein Muster gewählt</option>
         @foreach($Problems as $title)
           <option>{{{$title}}}</option>
         @endforeach
       </select>
+  </span>
+    <button type="submit" class="btn btn-primary" name="entryButton" value="newAssignment">Neuer Musterimpuls</button>
   </p>
   <p>
-      <label for="problem">Problem bearbeiten</label>
-
-  </p>
+    <div class="form-group">
+      <label for="problem">Schreibimpuls bearbeiten</label>
       <textarea class="form-control js-auto-size" id="problem" name="problem">{{$EntryInfo['problem']}}</textarea>
     </div>
   </p>
