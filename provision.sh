9df6090343060c886c40d101eeb3d8845a239d12
turbopope <PapstDonB@Googlemail.com>
2016-05-10 15:09:38 +0200

    Install gems via apt-get

--- provision.sh
+++ provision.sh
@@ -3,8 +3,8 @@
 apt-get update
 
 apt-get install -y fish tree
-/home/vagrant/.rbenv/shims/gem install sass
-/home/vagrant/.rbenv/shims/gem install dotenv
+
+apt-get install -y ruby-dotenv ruby-sass
 wget -qNO /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
 echo "Installed $(phpunit --version)"
