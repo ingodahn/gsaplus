805c6df140f0bc242372e6c715f38bc48f41481f
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-05-17 15:54:44 +0200

    Replace intro with the preproduction version

--- resources/assets/js/intro-modal.js
+++ resources/assets/js/intro-modal.js
@@ -1,5 +1,5 @@
 function startVideo() {
-  var src = 'https://www.youtube.com/embed/LS-VPyLaJFM?html5=1&autoplay=1';
+  var src = 'https://www.youtube.com/embed/15PsRxp3J_w?html5=1&autoplay=1';
   $('#intro-modal iframe').attr('src', src);
 }
 
