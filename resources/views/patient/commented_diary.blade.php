42a89dfada84a3c7861753215ec4d4569bd75011
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-05-24 18:46:55 +0200

    Change the structure of the page, add icon:

--- resources/views/patient/commented_diary.blade.php
+++ resources/views/patient/commented_diary.blade.php
@@ -11,7 +11,10 @@
         @endif
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.2/Chart.min.js"></script>
 
-        <h3>Meine Gesundheit</h3>
+        <h3><i class="fa fa-heartbeat" aria-hidden="true"></i> Befinden</h3>
+
+        <h4>Meine Gesundheit</h4>
+
         <canvas id="health" ></canvas>
         <script>
             var ctx = document.getElementById("health");
@@ -34,7 +37,8 @@
                 }});
         </script>
 
-        <h3>Meine Arbeitsfähigkeit</h3>
+        <h4>Meine Arbeitsfähigkeit</h4>
+
         <canvas id="wai" ></canvas>
         <script>
             var ctx = document.getElementById("wai");
