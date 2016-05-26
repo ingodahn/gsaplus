6a69025b54b4630b5eede8f250ce47ec6def57a5
turbopope <PapstDonB@Googlemail.com>
2016-05-26 09:47:45 +0200

    Remove unnecessary comment

--- provision.sh
+++ provision.sh
@@ -20,7 +20,6 @@ if [ ! -f /home/vagrant/provisioned ]; then
   chown -R vagrant:vagrant /var/www/piwik
   sed -i -e 's/;always_populate_raw_post_data/always_populate_raw_post_data/g' /etc/php5/apache2/php.ini
   sed -i '6iListen 8080' /etc/apache2/ports.conf
-  # Add /var/piwik as apache directory (?)
   cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/001-piwik.conf
   sed -i -e "s#DocumentRoot /var/www/public#DocumentRoot /var/www/gsa/public#g" /etc/apache2/sites-available/000-default.conf
   sed -i -e "s#DocumentRoot /var/www/public#DocumentRoot /var/www/gsa/public#g" /etc/apache2/sites-available/scotchbox.local.conf
