3a8ae955891f78461184abeced69eaa7b8393467
Sascha Zimmermann <zimsa@uni-koblenz.de>
2016-05-26 23:54:31 +0200

    change message reporting that all slots are occupied

--- resources/views/emails/no_more_time_slots.blade.php
+++ resources/views/emails/no_more_time_slots.blade.php
@@ -1,7 +1,8 @@
 <html>
     <body>
         <p>Sehr geehrter {{ $admin_name }},</p>
-        <p>bei einem Aufruf der Registrierung wurde festgestellt, dass der gewünschte Tag - {{ $day }} - bereits belegt ist.</p>
+        <p>für den {{ $day }} sind alle Slots belegt. Dies kann die Folge einer neuen Registrierung oder einer
+            Änderung des Schreibtages sein.</p>
         <p>Bitte erhöhen Sie bei Bedarf bzw. nach Möglichkeit die obere Grenze für die Registrierung.</p>
         <p>Viele Grüße,<br/>
             Ihr System GSA-Online plus</p>
