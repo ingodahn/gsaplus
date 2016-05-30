438817cf50854c05bb9fb6626cfe88d31119e94d
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-05-30 17:33:55 +0200

    Rework imprint and privacy information pages
    Remove privacy information from imprint
    Rewrite privacy information page based on the document by @dahn

--- resources/views/system/impressum.blade.php
+++ resources/views/system/impressum.blade.php
@@ -35,67 +35,7 @@
 
       <h3>Urheberrecht</h3>
       <p>
-
         Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
-      </p>
-
-
-      <hr>
-      <h2>Datenschutzerklärung</h2>
-
-      <h3>Datenschutz</h3>
-      <p>
-        Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechende der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.
-      </p>
-      <p>
-        Die Nutzung unserer Webseite ist ohne Angabe personenbezogener Daten möglich.
-      </p>
-      <p>
-        Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
-      </p>
-
-      <h3>Datenschutzerklärung für das Tracking von Nutzern</h3>
-      <p>
-        Aus den Daten können unter einem Pseudonym Nutzungsprofile erstellt werden. Dazu können Cookies eingesetzt werden. Bei Cookies handelt es sich um kleine Textdateien, die lokal im Zwischenspeicher Ihres Internet-Browsers gespeichert werden. Die Cookies ermöglichen es, Ihren Browser wieder zu erkennen. Die mit unseren Technologien erhobenen Daten werden ohne die gesondert erteilte Zustimmung des Betroffenen nicht genutzt, um Besucher unserer Website persönlich zu identifizieren und werden nicht mit personenbezogenen Daten über den Träger des Pseudonyms zusammengeführt.
-      </p>
-
-      <h3>Auskunft, Löschung, Sperrung</h3>
-      <p>
-        Sie haben jederzeit das Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck der Datenverarbeitung sowie ein Recht auf Berichtigung, Sperrung und Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit unter der im Impressum angegebenen Adresse an uns wenden.
-      </p>
-
-      <h3>Cookies</h3>
-      <p>
-        Die Internetseiten verwenden teilweise sogenannte Cookies. Cookies richten auf Ihrem Rechner keinen Schaden an und enthalten keine Viren. Cookies dienen dazu, unser Angebot nutzerfreundlicher, effektiver und sicherer zu machen. Cookies sind kleine Textdateien, die auf Ihrem Rechner abgelegt werden und die Ihr Browser speichert.
-      </p>
-      <p>
-        Die meisten der von uns verwendeten Cookies sind sogenannte „Session-Cookies“. Sie werden nach Ende Ihres Besuchs automatisch gelöscht. Andere Cookies bleiben auf Ihrem Endgerät gespeichert, bis Sie diese löschen. Diese Cookies ermöglichen es uns, Ihren Browser beim nächsten Besuch wiederzuerkennen.
-      </p>
-      <p>
-        Sie können Ihren Browser so einstellen, dass Sie über das Setzen von Cookies informiert werden und Cookies nur im Einzelfall erlauben, die Annahme von Cookies für bestimmte Fälle oder generell ausschließen sowie das automatische Löschen der Cookies beim Schließen des Browsers aktivieren. Bei der Deaktivierung von Cookies kann die Funktionalität dieser Website eingeschränkt sein.
-      </p>
-
-      <h3>Server-Log-Files</h3>
-      <p>
-        Der Provider der Seiten erhebt und speichert automatisch Informationen in sogenannte Server-Log-Files, die Ihr Browser automatische an uns übermittelt. Dies sind:
-      </p>
-      <ul>
-        <li>Browsertyp/ Browserversion</li>
-        <li>Verwendetes Betriebssystem</li>
-        <li>Referrer URL</li>
-        <li>Hostname des zugreifenden Rechners</li>
-        <li>Uhrzeit der Serveranfrage</li>
-      </ul>
-      <p>
-        Zusätzlich werden automatisiert weitere Daten zu Ihrem Nutzungsverhalten erhoben (z.B. Anzahl geschauter Videos oder Verweildauer auf der Internetplattform) und wissenschaftlich ausgewertet.
-      </p>
-
-      <h3>Erhebung personenbezogener Daten</h3>
-      <p>
-        Zur Nutzung unserer Internetplattform müssen Sie keine personenbezogenen Daten angeben. Die Anmeldung erfolgt mit dem Ihnen zugewiesenen Zugangscode. Nach der Anmeldung mittels Zugangscode erfolgt die Datenübertragung bei der Fragebogenerhebung verschlüsselt unter Verwendung einer 128 Bit SSL-Verschlüsselung (Secure Socket Layers), einem international anerkannten Sicherheitsstandard.
-      </p>
-      <p>
-        Von Seiten des Forscherteams werden wir Sie nie auffordern, Ihren Namen und Ihr Geburtsdatum anzugeben.
       </p>
 
     </div>
