4fec210ea06e63d6c21d6dea70b1c8e35f0039f5
turbopope <PapstDonB@Googlemail.com>
2016-06-08 21:30:12 +0200

    Change status-colors according to the new specification
    Refs #134.

--- resources/views/patient/diary.blade.php
+++ resources/views/patient/diary.blade.php
@@ -77,14 +77,14 @@
           $revealed = $isPatient && $current;
           $class = $current ? "diary-panel-current" : "diary-panel";
           switch($entry['entry_status_code']) {
-            case "E010": $displayState = "primary"; break; // Schreibimpuls nicht definiert
-            case "E015": $displayState = "primary"; break; // Schreibimpuls definiert
+            case "E010": $displayState = "default"; break; // Schreibimpuls nicht definiert
+            case "E015": $displayState = "default"; break; // Schreibimpuls definiert
             case "E020": $displayState = "primary"; break; // Schreibimpuls gegeben
             case "E030": $displayState = "primary"; break; // Tagebucheintrag bearbeitet
             case "E040": $displayState = "primary"; break; // Tagebucheintrag abgeschickt
             case "E050": $displayState = "primary"; break; // Tagebucheintrag mit Rückmeldung versehen
             case "E060": $displayState = "success"; break; // Rückmeldung bewertet
-            case "E070": $displayState = "warning"; break; // Tagebucheintrag überfällig
+            case "E070": $displayState = "primary"; break; // Tagebucheintrag überfällig
             default: $displayState = "default"; break;
           }
         ?>
