a3c10d839aca3f9c1f5031c66914e0fa565692ed
turbopope <PapstDonB@Googlemail.com>
2016-06-08 22:37:03 +0200

    Change status-colors according to Ingo's comment
    Refs #134.

--- resources/views/patient/status-color.blade.php
+++ resources/views/patient/status-color.blade.php
@@ -1,15 +1,16 @@
 <?php
 $displayState = "default";
 switch($statusCode) {
-  case "E010": $displayState = "default"; break; // Schreibimpuls nicht definiert
-  case "E015": $displayState = "default"; break; // Schreibimpuls definiert
-  case "E020": $displayState = "primary"; break; // Schreibimpuls gegeben
-  case "E030": $displayState = "primary"; break; // Tagebucheintrag bearbeitet
-  case "E040": $displayState = "primary"; break; // Tagebucheintrag abgeschickt
-  case "E050": $displayState = "primary"; break; // Tagebucheintrag mit Rückmeldung versehen
-  case "E060": $displayState = "success"; break; // Rückmeldung bewertet
-  case "E070": $displayState = "primary"; break; // Tagebucheintrag überfällig
-  default: $displayState = "default"; break;
+  case "E010": $displayState = "undefined"; break; // Schreibimpuls nicht definiert
+  case "E015": $displayState = "current"; break; // Schreibimpuls definiert
+  case "E020": $displayState = "current"; break; // Schreibimpuls gegeben
+  case "E030": $displayState = "current"; break; // Tagebucheintrag bearbeitet
+  case "E040": $displayState = "current"; break; // Tagebucheintrag abgeschickt
+  case "E050": $displayState = "current"; break; // Tagebucheintrag mit Rückmeldung versehen
+  case "E060": $displayState = "done"; break; // Rückmeldung bewertet
+  case "E070": $displayState = "overdue"; break; // Tagebucheintrag überfällig
+  case "E100": $displayState = "undefined"; break; // Tagebucheintrag überfällig
+  default: $displayState = "undefined"; break;
 }
 echo($displayState);
 ?>
