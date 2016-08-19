870b0adc9b3153d81833515877acdce8bdff0e43
ingodahn <ingodahn@gmail.com>
2016-08-19 12:00:15 +0200

    Added cronMail.blade.php

---
+++ resources/views/emails/cronMail.blade.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<p>Sehr geehrter Administrator,</p>
+<p>ich habe planmäßig gearbeitet und mich bemüht, eMails zu verschicken. Dabei habe ich auf <a href="{{ url("/") }}">unsere Website</a> verwiesen.
+</p>
+<p>Viele Grüße,<br/>
+    Ihr Cron-Job von GSA-Online plus</p>
+</body>
+</html>
