25f756bf195ab7c980a2c3b0ff66ef7db09b082b
Sergei Pachtchenko <gektor@uni-koblenz.de>
2016-08-19 12:21:30 +0200

    Fix typo

--- resources/views/emails/cronMail.blade.php
+++ resources/views/emails/cronMail.blade.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <p>Sehr geehrter Administrator,</p>
-<p>ich habe planmäßig gearbeitet und mich bemüht, eMails zu verschicken. Dabei habe ich auf <a href="{{ url("/") }}">unsere Website</a> verwiesen.
+<p>ich habe planmäßig gearbeitet und mich bemüht, E-Mails zu verschicken. Dabei habe ich auf <a href="{{ url("/") }}">unsere Website</a> verwiesen.
 </p>
 <p>Viele Grüße,<br/>
     Ihr Cron-Job von GSA-Online plus</p>
